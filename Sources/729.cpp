012817b663a9c41f9ec71c1ca3165a6942e04cd8
yeasin Rahaman <ryeashin@gmail.com>
2017-10-20 15:55:34 +0600

    Update 729.cpp

--- Sources/729.cpp
+++ Sources/729.cpp
@@ -1,3 +1,71 @@
+/*AC ON 0.000*/
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+
+using namespace std;
+
+bool taken[1234];
+char arr[1234],ans[1234];
+
+void backtrack(int n,int idx)
+{
+    if(idx == n){
+        //print the combination
+        ans[idx] = '\0';
+        puts(ans);
+
+        return;
+    }
+
+    for(int x = 0; arr[x]; x++)
+        if( !taken[x]){
+        ans[idx] = arr[x]; //save the combination here
+
+        taken[x] = true;
+        backtrack( n , idx+1);
+        taken[x] = false;
+
+        while( arr[x] == arr[x+1] ) x++;
+        }
+}
+
+
+int main()
+{
+//    freopen("input.txt","rt",stdin);
+//    freopen("out.txt","wt",stdout);
+
+    int dataset,T=0;
+
+    sf("%d",&dataset);
+
+    while(dataset--){
+        int N,H;
+
+        if(T>=1)puts("");
+        sf("%d%d",&N, &H);
+        for(int i=0; i<N; i++){
+            ans[i] = arr[i] = '0';taken[i] = 0;
+        }
+
+        for(int z = N-H; z<N; z++)arr[z] = '1';
+        arr[N] = '\0';
+        T++;
+
+        backtrack(N,0);
+
+
+    }
+
+    return 0;
+}
+
+
+
+/****************************/
+/***/****************************///
+
 #include<bits/stdc++.h>
 #define sf scanf
 #define pf printf
