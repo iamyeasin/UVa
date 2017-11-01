7c568f405a1eec9e4953eab57559ae8f64333399
yeasin Rahaman <ryeashin@gmail.com>
2017-11-01 18:42:43 +0600

    Create 725 Division.cpp

---
+++ Sources/725 Division.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+#define mx 1000000000
+
+using namespace std;
+
+int arr[14]; char ans[14];
+bool taken[14];
+vector < pair <string , long > > num;
+char ans2[1234];
+bool flag[12];
+
+bool isFound(string s, long test){
+    while(test){
+        int x = test%10;
+        char n = x + '0';
+
+        if(!flag[x])flag[x] = 1;
+        else return false;
+
+
+        for(int i=0; s[i]; i++ ){
+            if(s[i] == n){
+                return false;
+            }
+        }
+        test = test/10;
+    }
+
+    return true;
+}
+
+
+void backTrack(int idx, int n, int N){
+    if(idx == n) {
+        long nn = atol(ans);
+        long maal = N*nn;
+        int x = log10(maal);
+        if(x != 4)return;
+
+        if(isFound(ans,maal)){
+            num.push_back(make_pair(ans,maal ));
+        }
+        memset(flag,0,12);
+        return;
+    }
+
+    for(int i=0; i<10; i++){
+        if(ans[0] == '5')return;
+        if(!taken[i]){
+            taken[i] = 1;
+            ans[idx] = arr[i] + '0';
+            backTrack(idx+1,n, N);
+            taken[i] = false;
+        }
+
+    }
+
+}
+
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "rt", stdin);
+        freopen("out.cpp", "wt", stdout);
+    #endif // ONLINE_JUDGE
+
+    for(int i=0; i<10; i++) arr[i]=i;
+    long kase=1,Nm;
+
+    while( sf("%ld", &Nm) , Nm){
+        num.clear();
+        memset(taken,0,sizeof taken);
+        memset(ans,'\0',sizeof ans);
+
+        if(kase > 1)puts("");
+        kase++;
+
+        backTrack(0,5,Nm);
+        if(num.empty()) {
+            printf("There are no solutions for %ld.\n",Nm);
+            continue;
+        }
+
+        int sx =num.size();
+        for(int i=0; i<sx; i++){
+            cout << num[i].second << " / " << num[i].first << " = " << Nm << endl;
+        }
+
+
+    }
+
+    return 0;
+}
